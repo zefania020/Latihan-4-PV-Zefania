2b9ea4c4aff79f52280f9b15647df7b611c46f3d
zefania020 <85860150+zefania020@users.noreply.github.com>
2022-03-02 15:16:05 +0700

    Create Latihan4.cs
    Tugas Latihan 4

---
+++ Latihan4.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Latihan_4
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show ("Hello World " + this.textBox1.Text);			
+		}
+	}
+}
